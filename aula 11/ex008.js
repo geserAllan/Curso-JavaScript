a8eba8acca3be2a66e9c28eac7c6b09a0f469e6d
GeserAllan <geserallan@gmail.com>
2022-07-19 20:54:55 -0300

    condições if

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var _velocidade = 60.5
+console.log(`A velocidade do seu carro é ${_velocidade}Km/h`)
+if (_velocidade > 60) {
+    console.log('Você ultrapassou o limite permitido. Multado!')
+}
+console.log('Dirija sempre usando cinto de segurança')
