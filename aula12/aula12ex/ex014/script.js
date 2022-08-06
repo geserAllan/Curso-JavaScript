539d62027cfa2735fc052eb710780695b4e4cec1
GeserAllan <geserallan@gmail.com>
2022-08-06 16:27:17 -0300

    atualizar

--- aula12/aula12ex/ex014/script.js
+++ aula12/aula12ex/ex014/script.js
@@ -8,12 +8,15 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //"Bom dia"
+        document.body.style.background = '#00fff6'
         img.src = "amanhecer.png"
     }else if (hora >= 13 && hora <= 18) {
         //"Boa Tarde"
+        document.body.style.background = '#ff7838'
         img.src = "meiodia.png"
     }else {
         //"Boa Noite"
+        document.body.style.background = '#49007c'
         img.src = "anoitecer.png"
     }
         
