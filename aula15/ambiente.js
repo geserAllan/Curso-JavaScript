9c383c5e60c0adb6e39aa059d190b5b613d4edc0
GeserAllan <geserallan@gmail.com>
2022-08-10 20:09:38 -0300

    array

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -3,4 +3,8 @@ console.log(`Nossa array é ${num}`)*/
 
 
 let num = [5, 4 ,6, 2 ,8]
+num.push(3)
+num.sort()
 console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor é ${num[2]}`)
---
+++ aula15/vetornatela.js
@@ -0,0 +1,6 @@
+let valores = [ 3, 2, 5, 7, 4, 5,]
+
+//console.log(valores)
+for(let pos = 0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
