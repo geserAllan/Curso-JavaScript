76c1c0641389e086a2883eeed4a4481bebe049d9
GeserAllan <geserallan@gmail.com>
2022-08-10 20:19:28 -0300

    Update vetornatela.js

--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -1,6 +1,10 @@
 let valores = [ 3, 2, 5, 7, 4, 5,]
 
 //console.log(valores)
-for(let pos = 0; pos < valores.length; pos++){
+/*for(let pos = 0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}*/
+
+for(let pos in valores) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
