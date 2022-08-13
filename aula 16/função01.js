b277bb63af1f181295ca22ccc39957962a9f66bb
GeserAllan <geserallan@gmail.com>
2022-08-13 15:04:26 -0300

    Create função01.js

---
+++ aula 16/função01.js
@@ -0,0 +1,9 @@
+function parouImpar(numero) {
+    if(numero %2 == 0){
+        return 'Par'
+    }else {
+        return 'Impar'
+    }
+}
+//let resposta = parouImpar(3)
+console.log(parouImpar(3))
