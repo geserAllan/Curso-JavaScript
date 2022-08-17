661b2a4e3c75e27b926f2cd2e8716a368797b097
GeserAllan <geserallan@gmail.com>
2022-08-17 20:11:20 -0300

    verificador

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,4 +1,51 @@
 function verificar() {
-     window.alert('Funcionou')
+     var data = new Date()
+     var ano = data.getFullYear()
+     var fano = document.getElementById('txtano')
+     var res = document.querySelector('div#res')
+     if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[Erro!] Verifique os dados e tente novamente.')
+     }else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if(fsex[0].checked){
+            gênero = 'Homem'
+            if(idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'menino.png')
+            }else if(idade < 21) {
+                // Jovem
+                img.setAttribute('src','moço.png')
+            }else if(idade < 50) {
+                //adulto
+                img.setAttribute('src','homem.png')
+            }else {
+                //idoso
+                img.setAttribute('src','idoso.png')
+            }
+        }else if(fsex[1].checked){
+            gênero = 'Mulher'
+            if(idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'menina.png')
+            }else if(idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'moça.png')
+            }else if(idade < 50) {
+                //adulto
+                img.setAttribute('src', 'mulher.png')
+            }else {
+                //idoso
+                img.setAttribute('src', 'idosa.png')
+            }
+        }
+        
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade}`
+        res.appendChild(img)
+     }
 }
     
